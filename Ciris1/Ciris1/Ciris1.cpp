68988e2c661c473387bb56806470d31fafdf569b
Jake Alsemgeest <alsemgeester@gmail.com>
2015-02-17 12:37:30 -0500

    Creating a square on the screen. Slow update.

--- Ciris1/Ciris1/Ciris1.cpp
+++ Ciris1/Ciris1/Ciris1.cpp
@@ -1,25 +1,30 @@
 // Ciris1.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <Windows.h>
+#include <iostream>
+#include <string>
 
-int _tmain(int argc, _TCHAR* argv[])
+
+using namespace std;
+
+int main() //_tmain(int argc, _TCHAR* argv[])
 {
 
 	HDC hdc = GetDC(NULL);
 	while (1)
 	{
 		for (int i = 0; i < 800; i++) {
-			if (GetPixel(hdc, i, i) == RGB(100,100,200)) {
-
+			for (int j = 0; j < 800; j++) {
+				//if (GetPixel(hdc, i, i) == RGB(100,100,200)) {
+				//std::cout << "Got in here" << std::endl;
+				//}
+				SetPixel(hdc, i, j, RGB(100, 100, 200));
 			}
-			SetPixel(hdc, i, i, RGB(100,100,200));
 		}
 		Sleep(1);
 	}
-	return 0;
-
 	return 0;
 }
 
