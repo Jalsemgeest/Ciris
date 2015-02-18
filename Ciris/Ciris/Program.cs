f00f7a16a6b7438633d0241346ba10817f38fe46
Jake Alsemgeest <alsemgeester@gmail.com>
2015-02-17 20:21:54 -0500

    Initial Commit of Ciris v1.0
    This is a spin off of NegativeScreen.  It currently has the exact
    functionality of NegativeScreen and thereby doesn't quite need it all.
    Therefore it will be tweaked with and have much removed to ensure it is
    complete and fresh.

---
+++ Ciris/Ciris/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Ciris
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Adding a potential Exception to the program.
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            // Checking the bit mode of the OS.
+            if (NativeMethods.IsX86InWow64Mode())
+            {
+                System.Windows.Forms.MessageBox.Show(@"This was compiled for 32 bit processors.");
+                return;
+            }
+
+            // Checking windows version
+            if (Environment.OSVersion.Version < new Version(6, 1))
+            {
+                System.Windows.Forms.MessageBox.Show(@"This only works on Windows 7 and above.",
+                    "Warning",
+                    System.Windows.Forms.MessageBoxButtons.OK,
+                    System.Windows.Forms.MessageBoxIcon.Exclamation,
+                    System.Windows.Forms.MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            // Changes the working directory to be the one with the executable.
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            
+            // Initializes the configuration of the program.
+            Configuration.Initialize();
+
+            // Check if Aero is enabled.
+            if (Configuration.Current.ShowAeroWarning && !NativeMethods.DwmIsCompositionEnabled())
+            {
+                var result = System.Windows.Forms.MessageBox.Show("Windows Aero must be anled for this program.");
+                if (result != System.Windows.Forms.DialogResult.OK)
+                {
+                    return;
+                }
+            }
+
+            // Check if there is another instance running.
+            if (IsAnotherInstanceAlreadyRunning())
+            {
+                System.Windows.Forms.MessageBox.Show("The application is already running.",
+                    "Warning",
+                    System.Windows.Forms.MessageBoxButtons.OK,
+                    System.Windows.Forms.MessageBoxIcon.Exclamation,
+                    System.Windows.Forms.MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            NativeMethods.SetProcessDPIAware();
+
+            Application.EnableVisualStyles();
+            OverlayManager.Initialize();
+
+            Application.Run();
+        }
+
+        // Handling the custom exception for the application.
+        public static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString(), "Sorry, crashing...", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+        }
+
+        private static bool IsAnotherInstanceAlreadyRunning()
+        {
+            Process p = Process.GetCurrentProcess();
+
+            Process[] processWithSameName = Process.GetProcessesByName(p.ProcessName);
+
+            if (processWithSameName.Length == 1)
+            {
+                return false;
+            }
+
+            foreach (Process process in processWithSameName)
+            {
+                if (process.Id != p.Id && process.MainModule.FileName == p.MainModule.FileName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Ciris2/Ciris2/Ciris.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Ciris
+{
+
+    class Ciris
+    {
+        [STAThread]
+
+        static void Main(string[] args)
+        {
+
+
+            // Only 32 bit mode is allowed.
+            if (NativeMethods.IsX86InWow64Mode())
+            {
+                System.Windows.Forms.MessageBox.Show(@"Dang...");
+                return;
+            }
+
+            //check Windows version
+            //TODO: check whether the undocumented functions exist under Windows server 2008 (probably not) and R2 (probably yes)
+            if (Environment.OSVersion.Version < new Version(6, 1))
+            {
+                System.Windows.Forms.MessageBox.Show(@"Dang 2...",
+                 "Warning",
+                 System.Windows.Forms.MessageBoxButtons.OK,
+                 System.Windows.Forms.MessageBoxIcon.Exclamation,
+                 System.Windows.Forms.MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            
+
+        }
+    }
+
+}
