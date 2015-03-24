75c4ac092eb3d40a1d225dfcca95fc78db7d5d4c
Jake Alsemgeest <alsemgeester@gmail.com>
2015-03-24 08:56:57 -0400

    Updated the DLL - No error

--- Ciris/Ciris/Ciris.Designer.cs
+++ Ciris/Ciris/Ciris.Designer.cs
@@ -220,6 +220,7 @@
             // 
             // Ciris
             // 
+            // Disabling mimize and close
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
