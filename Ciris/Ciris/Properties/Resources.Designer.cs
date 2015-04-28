58fe8a04b9a3d62a35cba653c10d96c88e868619
Jake Alsemgeest <alsemgeester@gmail.com>
2015-04-28 13:09:49 -0400

    Close Button for Windows 8+

--- Ciris/Ciris/Properties/Resources.Designer.cs
+++ Ciris/Ciris/Properties/Resources.Designer.cs
@@ -63,6 +63,26 @@ namespace Ciris.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap background {
+            get {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cirislogo {
+            get {
+                object obj = ResourceManager.GetObject("cirislogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap deutranopia {
             get {
                 object obj = ResourceManager.GetObject("deutranopia", resourceCulture);
